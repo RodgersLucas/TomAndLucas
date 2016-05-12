3151e5cc8c0b2464ed65cb7fb874b1825256c01e
Tom Hall <thall1961@gmail.com>
2016-05-12 14:24:16 -0700

    Started opening menu

--- DeliveryGame/src/deliverygame/DeliveryGame.java
+++ DeliveryGame/src/deliverygame/DeliveryGame.java
@@ -9,13 +9,31 @@ package deliverygame;
  *
  * @author Tom Hall and Lucas Rodgers
  */
-public class DeliveryGame {
 
+import java.util.Scanner;
+
+public class DeliveryGame {
+    
+    
+    
+    // Creates timer variable (used later in the streets) and
+    // numOfStreets which shows all of the options a user could
+    // pick.  Both are constants (but don't have to be?)
     static private double timer = 10000;
     static private int numOfStreets = 12;
+    private  static String choice;
     
+    //Method returns number of streets available
     static public int getNumOfStreets(){
         return numOfStreets;
+    }
+    
+    static public void setChoice( String userChoice ){
+        DeliveryGame.choice = userChoice;
+    }
+    
+    static public String getChoice (){
+        return choice;
     }
     
     public static void main(String[] args) {
@@ -23,10 +41,20 @@ public class DeliveryGame {
                 + "There are " + DeliveryGame.getNumOfStreets() + " streets that you could go down!\n"
                 + "Have fun!!!");
         
-        System.out.println("\n\nWhat would you like to do?\n"
+        //Loops through menu until a correct value is chosen
+        do{System.out.println("\n\nWhat would you like to do?\n"
                 + "1. Pick A City\n"
                 + "2. See High Scores\n"
-                + "3. Quit");
+                + "3. Quit\n"
+                + "(Enter 1, 2, or 3)");
+        
+            Scanner input = new Scanner(System.in);
+            DeliveryGame.setChoice( input.next());} 
+        while(!"1".equals(DeliveryGame.getChoice()) && !"2".equals(DeliveryGame.getChoice()) && !"3".equals(DeliveryGame.getChoice()));
+        //End of menu loop
+        
+        System.out.println(DeliveryGame.getChoice());
+        
     }
     
 }
