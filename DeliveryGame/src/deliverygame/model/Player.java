f5465d72054a20df3f1838834eb7e52651e08d14
Tom Hall <thall1961@gmail.com>
2016-05-12 20:58:50 -0700

    Added attributes and methods to Player class

--- DeliveryGame/src/deliverygame/model/Player.java
+++ DeliveryGame/src/deliverygame/model/Player.java
@@ -10,5 +10,22 @@ package deliverygame.model;
  * @author rjhall7
  */
 public class Player {
+    private static String name;
+    private static double bestTime;
     
+    public static void setName( String newName ){
+        name = newName;
+    }
+    
+    public static String getName(){
+        return name;
+    }
+    
+    public static void setTime ( double newBestTime ){
+        bestTime = newBestTime;
+    }
+    
+    public static double getTime () {
+        return bestTime;
+    }
 }
